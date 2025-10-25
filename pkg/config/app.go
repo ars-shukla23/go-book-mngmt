6bff448201da9b3b80beeaf5ad38401fae1d6243
Aayush Shukla <aayush.shukla23@gmail.com>
2025-10-26 00:39:32 +0700

    Added routes and db connector function

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,23 @@
 package config
+
+//The whole point of this file will be to return a variable DB which will help the other files interact with the database
+
+
+import(
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+
+func Connect(){
+	d, err:=gorm.Open("mysql","")
+	if err!=nil{
+		panic(err)
+	}
+	db=d
+}
+func getDB() *gorm.DB{
+	return db
+}
+
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import(
+	"github.com/gorilla/mux"
+	"github.com/ars-shukla23/go-book-mngmt/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes=func(router *mux.Router){
+	router.HandleFunc("/book/",controllers.GetAllBooks).Methods("GET")
+	router.HandleFunc("/book/",controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/{bookId}",controllers.GetBookByID).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}",controllers.DeleteBook).Methods("DELETE")
+	
+}
