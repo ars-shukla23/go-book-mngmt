afbcd59c280fd9e71716a9d4dbc909c0cbc9cbe0
Aayush Shukla <aayush.shukla23@gmail.com>
2025-10-31 19:31:29 +0700

    Created main.go, assigning router using BookStoreRoutes func defined in Routes and starting the server using ListenAndServe

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,5 +6,11 @@ import(
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/ars-shukla23/go-book-mngmt/pkg/routes"
-
 )
+
+func main(){
+	r:=mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010",r))
+}
