fd6083579d8b2fd9e54735f3ac3b75d214367ca8
Aayush Shukla <aayush.shukla23@gmail.com>
2025-10-31 19:22:46 +0700

    Defined Book Model and created DB init function

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,10 @@
 package main
+
+import(
+	"net/http"
+	"log"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/ars-shukla23/go-book-mngmt/pkg/routes"
+
+)
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,21 @@
 package models
+
+import(
+	"github.com/jinzhu/gorm"
+	"github.com/ars-shukla23/go-book-mngmt/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct{
+	gorm.Model
+	Title string `gorm:"" json:"title"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init(){
+	config.Connect()
+	db=config.getDB()
+	db.AutoMigrate(&Book{})
+}
